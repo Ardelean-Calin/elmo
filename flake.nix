83aae71dbcc0d8624414b86d19ccc8508c474695
Ardelean Calin <9417983+Ardelean-Calin@users.noreply.github.com>
2024-03-17 18:50:37 +0200

    Added devenv for ease of build.

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    systems,
+    ...
+  } @ inputs: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    packages = forEachSystem (system: {
+      devenv-up = self.devShells.${system}.default.config.procfileScript;
+    });
+
+    devShells =
+      forEachSystem
+      (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            {
+              languages.go.enable = true;
+              # https://devenv.sh/reference/options/
+              packages = [pkgs.zig];
+
+              scripts.build.exec = ''
+                GOOS="linux" GOARCH="amd64" CC="${pkgs.zig}/bin/zig cc -target x86_64-linux-musl" CXX="${pkgs.zig}/bin/zig c++ -target x86_64-linux-musl" go build
+              '';
+            }
+          ];
+        };
+      });
+  };
+}
